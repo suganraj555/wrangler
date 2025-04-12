8ad74e335618300182e24574cf8b7622bc583518
suganraj555 <105486612+suganraj555@users.noreply.github.com>
2025-04-12 18:49:03 +0530

    DirectiveVisitor.java

--- wrangler-core/src/main/antlr4/io/cdap/wrangler/parser/DirectiveVisitor.java
+++ wrangler-core/src/main/antlr4/io/cdap/wrangler/parser/DirectiveVisitor.java
@@ -1,9 +1,23 @@
-@Override
-public Token visitByteSizeArg(DirectivesParser.ByteSizeArgContext ctx) {
-    return new ByteSize(ctx.getText());
-}
+package io.cdap.wrangler.grammar;
 
-@Override
-public Token visitTimeDurationArg(DirectivesParser.TimeDurationArgContext ctx) {
-    return new TimeDuration(ctx.getText());
+import io.cdap.wrangler.api.parser.ByteSize;
+import io.cdap.wrangler.api.parser.TimeDuration;
+import io.cdap.wrangler.api.parser.Token;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+public class RecipeVisitor extends DirectiveBaseVisitor<Token> {
+
+    @Override
+    public Token visitByteSizeArg(DirectivesParser.ByteSizeArgContext ctx) {
+        // Visit ByteSize arguments and return a new ByteSize token
+        return new ByteSize(ctx.getText());
+    }
+
+    @Override
+    public Token visitTimeDurationArg(DirectivesParser.TimeDurationArgContext ctx) {
+        // Visit TimeDuration arguments and return a new TimeDuration token
+        return new TimeDuration(ctx.getText());
+    }
+
+    // Other visit methods for other constructs can be added here
 }
